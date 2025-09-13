caf3c610cf50171a44c52d4b21f861fce5e3e58e
jcatillo <jcatillo1121@gmail.com>
2025-09-13 15:26:27 +0800

    chore: code cleanup

--- Atillo/student-api/app/Services/StudentService.php
+++ Atillo/student-api/app/Services/StudentService.php
@@ -7,8 +7,8 @@ use App\Models\Student;
 class StudentService{
     public function getAllStudents() { return Student::all(); }
     public function getStudentById($id) { return Student::find($id); }
-    public function addStudent(array $data) { return Student::create($data); }
-    public function updateStudent($id, array $data){
+    public function addStudent($data) { return Student::create($data); }
+    public function updateStudent($id, $data){
         $student = Student::find($id);
         if(!$student) return null;
         $student->update($data);
